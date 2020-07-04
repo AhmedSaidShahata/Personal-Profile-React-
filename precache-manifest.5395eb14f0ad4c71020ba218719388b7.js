680cc5b0906637be6cea25b0985e63c054807b96
AhmedSaidShahata <ahmedsaidshehata1@gmail.com>
2020-07-04 22:26:13 +0200

    Updates

---
+++ precache-manifest.5395eb14f0ad4c71020ba218719388b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cf3d588282602ecad37d45079cbd870",
+    "url": "/PersonalProfile/index.html"
+  },
+  {
+    "revision": "b3b1c7663d521306ad63",
+    "url": "/PersonalProfile/static/css/main.6ff1054e.chunk.css"
+  },
+  {
+    "revision": "a1f92aa1c9bd62d81eeb",
+    "url": "/PersonalProfile/static/js/2.d0296670.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PersonalProfile/static/js/2.d0296670.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3b1c7663d521306ad63",
+    "url": "/PersonalProfile/static/js/main.2e7c408b.chunk.js"
+  },
+  {
+    "revision": "7649d52c630c0531db49",
+    "url": "/PersonalProfile/static/js/runtime-main.8907d454.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PersonalProfile/static/media/logo.5d5d9eef.svg"
+  }
+]);
